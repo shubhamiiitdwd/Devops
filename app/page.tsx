ae1f817babef087e931f65e0cf31e76100189492
shubhamiiitdwd <21bds063@iiitdwd.ac.in>
2024-10-23 19:21:36 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen justify-center items-center flex">
+      <h1 className="text-4xl text-white">Hello, This is Shubham</h1>
+    </div>
+  );
+}
